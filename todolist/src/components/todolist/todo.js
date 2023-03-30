bbf146d1042f4459b91a81cd738b9dba62803603
Arafathislam <islamarafath315@gmail.com>
2023-03-30 16:27:51 +0600

    working

---
+++ todolist/src/components/todolist/todo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./style.css"
+const todo = () => {
+  return (
+    <>
+    <div className='main-div'>
+        <div className='child-div'>
+            <figure>
+                <img src="./images/todo.svg" alt=""/>
+            </figure>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default todo
